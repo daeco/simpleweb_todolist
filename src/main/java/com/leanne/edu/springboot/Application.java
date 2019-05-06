84a0978340d56c46a782d9b2b12099b0f838587f
daeco <rlaeogh89@gmail.com>
2019-05-06 13:48:23 +0900

    [H2 JPA] DAO 테스트

--- src/main/java/com/leanne/edu/springboot/Application.java
+++ src/main/java/com/leanne/edu/springboot/Application.java
@@ -1,12 +1,32 @@
 package com.leanne.edu.springboot;
 
+import com.leanne.edu.springboot.dao.UserDAO;
+import com.leanne.edu.springboot.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SpringApplication.run(Application.class, args);
+
+        UserDAO dao = new UserDAO();
+        User user = new User();
+        user.setId("whatsup");
+        user.setName("갓준형");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
     }
 }
--- src/main/java/com/leanne/edu/springboot/configuration/H2Configuraion.java
+++ src/main/java/com/leanne/edu/springboot/configuration/H2Configuraion.java
@@ -1,18 +1,19 @@
+/*
 package com.leanne.edu.springboot.configuration;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class H2Configuraion {
-    static final String h2WebConsoleUrl = "/console/*";
 
     @Bean
     ServletRegistrationBean h2ServletRegistration() {
         ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-        registration.addUrlMappings(h2WebConsoleUrl);
+        registration.addUrlMappings("/console/*");
         return registration;
     }
 }
+*/
