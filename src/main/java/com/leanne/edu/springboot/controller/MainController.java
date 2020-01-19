efbf019956ba2dbd5adde01c1578405c66a943cc
daeco <rlaeogh89@gmail.com>
2020-01-19 23:01:06 +0900

    [todo] UI 수정 및 할일 등록 REST API 생서

--- src/main/java/com/leanne/edu/springboot/controller/MainController.java
+++ src/main/java/com/leanne/edu/springboot/controller/MainController.java
@@ -1,28 +1,38 @@
 package com.leanne.edu.springboot.controller;
 
-import com.leanne.edu.springboot.dao.TodoDAO;
-import com.leanne.edu.springboot.model.Todo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.PostConstruct;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.leanne.edu.springboot.dao.TodoDAO;
+import com.leanne.edu.springboot.model.Todo;
 
 @Controller
 public class MainController {
     @Autowired
     TodoDAO todoDAO;
+    
+    
 
     @PostConstruct
     public void init() {
         try {
             todoDAO.insertTodo("집안일", null);
-            todoDAO.insertTodo("짤래", "1");
-            todoDAO.insertTodo("청서", "1");
-            todoDAO.insertTodo("방청소", "1|3");
+            todoDAO.insertTodo("빨래", null);
+            todoDAO.insertTodo("청소", null);
+            todoDAO.insertTodo("방청소", null);
         } catch (ClassNotFoundException | SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -30,7 +40,7 @@ public class MainController {
     }
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
         List<Todo> todoList = new ArrayList<>();
         try {
             todoList = todoDAO.selectAll();
@@ -39,7 +49,15 @@ public class MainController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        model.addAttribute("todolist", todoList);
 
         return "index";
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> addWork(@Valid @RequestBody Todo todo, Errors errors) {
+    	
+    	return ResponseEntity.ok(null);
     }
 }
